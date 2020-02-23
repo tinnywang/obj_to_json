53bf08d76d0d3a6dd7829f68c74d5c12a8a151e7
Tiffany Wang <tinnywang@gmail.com>
2020-02-23 01:33:55 -0800

    parse materials

--- material.cpp
+++ material.cpp
@@ -2,9 +2,87 @@
 #include "parse.h"
 
 #include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "nlohmann/json.hpp"
 
 namespace material {
-material parse(const std::string &filename) {
+std::map<std::string, material> parse(const std::string &filename) {
   std::ifstream file = open(filename);
+  std::string line;
+  std::stringstream ss;
+  std::string definition;
+  std::map<std::string, material> materials;
+
+  while (std::getline(file, line)) {
+    ss = std::stringstream(line);
+    ss >> definition;
+    material _m;
+    material &m = _m;
+
+    try {
+      switch (definitions.at(definition)) {
+      case newmtl: {
+        ss >> m.name;
+        materials.emplace(m.name, m);
+        break;
+      }
+      case Ka: {
+        parse(ss, m.ambient);
+        break;
+      }
+      case Kd: {
+        parse(ss, m.diffuse);
+        break;
+      }
+      case Ks: {
+        parse(ss, m.specular);
+        break;
+      }
+      case Ns: {
+        ss >> m.specular_exponent;
+        break;
+      }
+      case d:
+      case Tr: {
+        break;
+      }
+      case illum: {
+        break;
+      }
+      }
+    } catch (std::out_of_range) {
+      // ignore any unsupported definitions
+    }
+  }
+
+  return materials;
+}
+
+color &parse(std::stringstream &ss, color &c) {
+  ss >> c.r >> c.g >> c.b;
+  return c;
+}
+
+void to_json(nlohmann::json &j, const material &m) {
+  j = nlohmann::json{
+      {"name", m.name},
+      {"ambient", m.ambient},
+      {"diffuse", m.diffuse},
+      {"specular", m.specular},
+      {"specular_exponent", m.specular_exponent},
+  };
+}
+
+void to_json(nlohmann::json &j, const color &c) {
+  std::vector<double> rgb = {c.r, c.g, c.b};
+  j = nlohmann::json(rgb);
+}
+
+void write(std::map<std::string, material> materials, std::ostream &out) {
+  out << nlohmann::json(materials) << std::endl;
 }
 }; // namespace material
--- material.h
+++ material.h
@@ -1,9 +1,29 @@
+#include <map>
 #include <string>
+#include <vector>
+
+#include "nlohmann/json.hpp"
 
 #ifndef INCLUDE_MATERIAL_H
 #define INCLUDE_MATERIAL_H
 
 namespace material {
+enum definition {
+  newmtl, // newmtl
+  Ka,     // ambient
+  Kd,     // diffuse
+  Ks,     // specular
+  Ns,     // specular exponent
+  d,      // transparency
+  Tr,     // transparency (Tr = 1 - d)
+  illum,  // illumination model
+};
+
+static std::map<const std::string, definition> definitions = {
+    {"newmtl", newmtl}, {"Ka", Ka}, {"Kd", Kd}, {"Ks", Ks},
+    {"Ns", Ns},         {"d", d},   {"Tr", Tr}, {"illum", illum},
+};
+
 struct color {
   double r, g, b;
 };
@@ -13,10 +33,14 @@ struct material {
   color ambient;
   color diffuse;
   color specular;
-  color specular_exponent;
+  double specular_exponent;
 };
 
-material parse(const std::string &filename);
+std::map<std::string, material> parse(const std::string &filename);
+color &parse(std::stringstream &ss, color &c);
+void to_json(nlohmann::json &j, const material &m);
+void to_json(nlohmann::json &j, const color &c);
+void write(std::map<std::string, material> materials, std::ostream &out);
 }; // namespace material
 
 #endif // INCLUDE_MATERIAL_H
